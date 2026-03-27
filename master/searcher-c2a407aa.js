5ea9bd22dca68a1f52bc52ed62c5992faa122f90
mhersson <mhersson@users.noreply.github.com>
2026-03-27 08:42:01 +0000

    deploy: 70ff6fce6de0d9d8e1f356524bcedef2e5369925

--- master/searcher-c2a407aa.js
+++ master/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5566e131.js',
+                path_to_root + 'searchindex-4223ba3d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
